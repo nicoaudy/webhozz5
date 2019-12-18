97e20a05a1b43a7fde7e137d3e5a58f130588b8f
Cody Mac <nicoaudy1994@gmail.com>
2019-12-18 20:43:19 +0700

    sending variable to view

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,1 +1,3 @@
  <h1>Hello from test</h1>
+{{ json_encode($language) }} <br>
+{{ $message }}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,18 @@
 <?php
 
+// HTTP VERB :
+// GET
+// POST
+// PUT/PATCH
+// DELETE
 
 Route::get('/', function () {
-	return view('test');
+	$message = "But Laravel is awesome 🔥";
+	$language = ['PHP', 'Javascript', 'JAVA', 'Python', 'Golang'];
+	return view('test', [
+		'language' => $language,
+		'message' => $message
+	]);
 });
 
 Route::get('about', function () {
