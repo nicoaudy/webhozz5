79fa03acda2fda1414f19ae4f4eecca85f165b1f
Nico Audy <nicoaudy1994@gmail.com>
2020-01-06 20:03:54 +0700

    initial commit product rest

--- routes/web.php
+++ routes/web.php
@@ -19,17 +19,19 @@ Route::put('/category/{id}', 'CategoryController@update');
 // Delete db
 Route::delete('/category/{id}', 'CategoryController@destroy');
 
+Route::resource('products', 'ProductController');
+
 Route::get('/', function () {
-	$message = "But Laravel is awesome 🔥";
-	$language = ['PHP', 'Javascript', 'JAVA', 'Python', 'Golang'];
-	$number = 10 * 5;
-	return view('test', [
-		'language' => $language,
-		'message' => $message,
-		'number' => $number
-	]);
+    $message = "But Laravel is awesome 🔥";
+    $language = ['PHP', 'Javascript', 'JAVA', 'Python', 'Golang'];
+    $number = 10 * 5;
+    return view('test', [
+        'language' => $language,
+        'message' => $message,
+        'number' => $number
+    ]);
 });
 
 Route::get('about', function () {
-	return 'from about';
+    return 'from about';
 });
